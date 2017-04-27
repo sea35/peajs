1fed89338dd74910b05709ddd08e8da25393a502
sea35 <sea35@163.com>
2017-04-27 16:36:23 +0800

    增加代理服务

--- proxy.config.js
+++ proxy.config.js
@@ -3,33 +3,12 @@
  */
 'use strict';
 
-//const integralHosts="http://172.16.33.167:8001";
-const integralHosts="http://172.16.34.41:11010"//"http://172.16.34.41:9061"
-const userHosts="http://172.16.34.41:9090"
-/*const ranchHosts="http://172.16.34.41:9090"*/
-const docHosts="http://172.16.34.41:9090"
+const integralHosts="https://www.baidu.com";
 
-const productHosts="http://172.16.34.41:9020"
-const serviceHosts="http://172.16.34.41:9010"
-const devidedHosts="http://172.16.34.41:9012"
 
-const mock = {
-
-    'POST /integral/(.*)': integralHosts,
-    'POST /user/*': userHosts,
-    /* 'POST /ranch/!*': ranchHosts,*/
-    'POST /doc/*': docHosts,
-
-    //商品管理，服务管理,设备管理,图片上传
-    'POST /product/*':productHosts,
-    'POST /service/*':serviceHosts,
-    'POST /divided/*':devidedHosts,
-    /* 图片上传格式
-     该格式比较特殊，请勿随便更改，调用地址:http://8098.hc.dev.zoenet.cn/commons/ctrl-http/upload
-     前端图片控件绑定如：action="/upload"
-     */
-    'POST /upload':'http://8098.hc.dev.zoenet.cn/commons/ctrl-http/',
+const proxy = {
+    '/baidu/(.*)': integralHosts,
 }
 
 
-module.exports = mock;
+module.exports = proxy;
